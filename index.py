f63fc6fda0ee4b07617282f3101db1ef872e0ff3
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-16 17:25:59 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,4 @@
+peso = float(input("Cual es tu peso?")) # Cual es tu peso? ex 76
+estatura = float(input("Cual es tu estatura?")) # Cual es tu estatura? ex 1.79
+imc = peso / (estatura * 100) * (estatura * 100)
+print(imc) # 76.0
